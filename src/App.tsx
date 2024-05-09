ceaef218b2b340214636c57ab153d98580cde841
Maria Freitas <mariaeduardamartinsf@gmail.com>
2024-05-09 16:12:08 -0300

    feat: criacao de paginas inciais, instalcao de libs e criacao de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
+import Register from "./pages/Register/Register";
+import Characters from "./pages/Characters/Characters";
+import Comics from "./pages/Comics/Comics";
+import Movies from "./pages/Movies/Movies";
+
 function App() {
-  return <div>oie jesus</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Register />} />
+
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/comics" element={<Comics />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
